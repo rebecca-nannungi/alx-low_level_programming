fb8b6c2c8cdbe393ac7312d7ad6808f8d258daf6
rebecca-nannungi <79253535+rebecca-nannungi@users.noreply.github.com>
2023-02-24 22:48:20 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
- */
+ * print_numbers - print 0-9
+ * Return: nothing
+ **/
+
 void print_numbers(void)
 {
-int c;
+	char i;
 
-for (c = 48; c < 58; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
