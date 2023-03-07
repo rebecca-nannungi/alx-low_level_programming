9f85a99b0bfd77f780b82aac9ac55f1c86362166
Rebecca Nannungi <rebeccanannungi@gmail.com>
2023-03-06 17:16:20 -0800

    push

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,27 +1,22 @@
 #include "main.h"
-
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string to search
- * @accept: set of bytes to search for
- * Description: retruns first occurrence in a string
- * Return: pointer to first instance of string
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
 **/
-
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (accept[j] == s[i])
-			{
-				return (&s[i]);
-			}
-		}
-	}
-	return (NULL);
+                int k;
+                while (*s)
+                {
+                        for (k = 0; accept[k]; k++)
+                        {
+                        if (*s == accept[k])
+                        return (s);
+                        }
+                s++;
+                }
+        return ('\0');
 }
 
