164ddd25a9f46cb2c249e36fc39656f93d111a59
Rebecca Nannungi <rebeccanannungi@gmail.com>
2023-03-08 11:48:43 -0800

    pray

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,15 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
-{
-	_putchar('\n');
-}
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 
 }
 
