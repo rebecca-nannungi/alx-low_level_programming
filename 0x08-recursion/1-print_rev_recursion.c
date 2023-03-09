fd7cd05e88f36c7500d58266f4e9885ddad131e7
Rebecca Nannungi <rebeccanannungi@gmail.com>
2023-03-09 07:06:31 -0800

    r

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - lenght of string
- * @s: string
- * Description:lenght of string
- * Return: int lenght
+ * _print_rev_recursion - print in reverse
+ * @s: input string
+ * Description: print string in reverse
+ * Return:nothing
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	if (*(s + 1) == '\0')
+	if (s[0] == '\0')
+		return;
+
+	if (s[1] != '\0')
 	{
-		return (1);
+		_print_rev_recursion(&s[1]);
+		_putchar(s[0]);
 	}
 	else
 	{
-		return (_strlen_recursion(s + 1) + 1);
+		_putchar(s[0]);
 	}
 }
 
