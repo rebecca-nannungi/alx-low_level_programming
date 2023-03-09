d63b78d5822c37527a35df57fb2baf4076586630
Rebecca Nannungi <rebeccanannungi@gmail.com>
2023-03-09 06:02:40 -0800

    ill

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
+ * _puts_recursion - prints string
+ * @s: input string
+ * Description: prints string
+ * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
-	{
 		_putchar('\n');
-	}
-
 }
 
