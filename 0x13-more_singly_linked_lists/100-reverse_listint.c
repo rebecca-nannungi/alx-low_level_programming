77735b8b5d791b7c460fdba47bdb345b1c5f1a5a
Rebecca Nannungi <rebeccanannungi@gmail.com>
2023-04-05 13:23:50 -0700

    list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a listing_t list
+ * @head: pointer to head of list
+ * Return: address of new head, NULL if fails
+ **/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prior, *next;
+
+	if (*head == NULL)
+		return (NULL);
+
+	prior = *head;
+	next = (*head)->next;
+	prior->next = NULL;
+
+	while (next != NULL)
+	{
+		*head = next;
+		next = next->next;
+		(*head)->next = prior;
+		prior = *head;
+	}
+
+	return (*head);
+}
+
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find a loop in a listint_t list
+ * @head: pointer to head of list
+ * Return: pointer to start of loop, NULL if no loop
+ **/
+listint_t *find_listint_loop(listint_t *head)
+{
+	head = head;
+	return (NULL);
+}
+
