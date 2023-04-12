6906248f6dea6ec35e78ef38dcc303dfa817c570
Rebecca Nannungi <rebeccanannungi@gmail.com>
2023-04-12 02:58:18 -0700

    bits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,49 @@
 #include "main.h"
 
 /**
- * binary_to_unit -converts a binary number to an unsigned int.
- * @b: pointing to a string of 0 and 1 chars
- *
- * Return:the converted number or 0
+ * power- calculates power of 2
+ * @n: power of 2
+ * Return: result of powering 2
  */
+
+unsigned int power(int n)
+{
+	unsigned int result = 1;
+
+	if (n == 0)
+		return (1);
+	result = 1 << n;
+	return (result);
+}
+/**
+ * binary_to_uint- converts binary to integer
+ * @b: character pointer to string of binary
+ * Return: unsigned int decimal
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int j;
+	int count = 0, d = 0;
+	unsigned int number = 0, c;
 
-	j = 0;
-	if (!b)
+	if (b == NULL)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	while (b[d] != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[d] !=  '0' && b[d] != '1')
 			return (0);
+		d++;
 	}
-	for (i = 0; b[i] != '\0'; i++)
+	while (d >= 0)
 	{
-		j <<= 1;
-		if (b[i] == '1')
-			j += 1;
+		if (b[d] == '1')
+			c = 1;
+		else
+			c = 0;
+		number += c * power(count - 1);
+		count++;
+		d--;
 	}
-	return (j);
+	return (number);
 }
 
